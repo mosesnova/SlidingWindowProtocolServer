55a68cf691980fa69cbb829bd4d11aad535bec4f
Moses Nova <moses@npl.nz>
2023-02-13 14:16:34 +1300

    comit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
 Console.WriteLine("Hello, World!");
+var ipEndPoint = new IPEndPoint(IPAddress.Any, 13);
+TcpListener listener = new(ipEndPoint);
+
+try
+{
+    listener.Start();
+
+    using TcpClient handler = await listener.AcceptTcpClientAsync();
+    await using NetworkStream stream = handler.GetStream();
+
+    var message = $"📅 {DateTime.Now} 🕛";
+    var dateTimeBytes = Encoding.UTF8.GetBytes(message);
+    await stream.WriteAsync(dateTimeBytes);
+
+    Console.WriteLine($"Sent message: \"{message}\"");
+    // Sample output:
+    //     Sent message: "📅 8/22/2022 9:07:17 AM 🕛"
+}
+finally
+{
+    listener.Stop();
+}
